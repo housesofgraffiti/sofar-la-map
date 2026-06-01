0a497e6397f896eb37e5108cc7a324f24df56911
housesofgraffiti <ben.negron@sofarsounds.com>
2026-06-01 16:53:35 -0700

    Create airtable-proxy.js

---
+++ netlify/functions/airtable-proxy.js
@@ -0,0 +1,55 @@
+exports.handler = async function(event) {
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type, x-airtable-token',
+        'Access-Control-Allow-Methods': 'GET, OPTIONS'
+      },
+      body: ''
+    };
+  }
+
+  const token = event.headers['x-airtable-token'];
+  if (!token) {
+    return {
+      statusCode: 401,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({ error: { message: 'Missing x-airtable-token header' } })
+    };
+  }
+
+  const {
+    baseId = 'appFjmrBPpOKVSdpz',
+    table = 'Events',
+    pageSize = '100',
+    offset
+  } = event.queryStringParameters || {};
+
+  const params = new URLSearchParams({ pageSize });
+  if (offset) params.append('offset', offset);
+
+  const url = `https://api.airtable.com/v0/${baseId}/${encodeURIComponent(table)}?${params}`;
+
+  try {
+    const res = await fetch(url, {
+      headers: { 'Authorization': `Bearer ${token}` }
+    });
+    const data = await res.json();
+    return {
+      statusCode: res.status,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify(data)
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({ error: { message: err.message } })
+    };
+  }
+};
